0fcb60ce012f038e9c03f4da676168d9bd05c13c
Jared Baur <jaredbaur@fastmail.com>
2025-07-17 07:38:00 -0700

    Add nixos module for ensuring quartus tools are in PATH

--- flake.nix
+++ flake.nix
@@ -5,6 +5,21 @@
     overlays.default = final: prev: {
       quartus-pro-programmer = final.callPackage ./package.nix { };
     };
+    nixosModules.default =
+      {
+        config,
+        lib,
+        pkgs,
+        ...
+      }:
+      {
+        options.programs.quartus-pro-programmer.enable = lib.mkEnableOption "quartus-pro-programmer";
+        config = lib.mkIf config.programs.quartus-pro-programmer.enable {
+          nixpkgs.overlays = [ inputs.self.overlays.default ];
+          environment.systemPackages = [ pkgs.quartus-pro-programmer ];
+          environment.profileRelativeSessionVariables.PATH = [ "/qprogrammer/quartus/bin" ];
+        };
+      };
     legacyPackages = inputs.nixpkgs.lib.genAttrs [ "x86_64-linux" ] (
       system:
       import inputs.nixpkgs {
